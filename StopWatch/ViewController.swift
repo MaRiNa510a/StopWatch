8d970b6466b3e0e205eb09669f34d7568e19d73b
Marina Goto <5m.clover3@gmail.com>
2018-06-20 17:31:49 +0900

    1

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var count: Float = 0.0
+    
+    var timer:Timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +24,28 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func up() {
+        count = count + 0.01
+        label.text = String(format: "%.2f", count)
+    }
+    
+    @IBAction func start() {
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                         target: self,
+                                         selector: #selector(self.up),
+                                         userInfo: nil,
+                                         repeats: true)
+        }
+    }
+    
+    @IBAction func stop() {
+        if timer.isValid {
+            timer.invalidate()
+        }
+        
     }
 
 
